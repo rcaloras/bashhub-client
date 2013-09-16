9bb10e605104ae2b187fe243e7874ee0a59dbd02
Ryan Caloras <rcaloras@gmail.com>
2013-09-16 00:48:31 -0400

    Created session model

--- src/python/bashhub.py
+++ src/python/bashhub.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from bson.objectid import ObjectId
-from time import time
+from time import *
 import jsonpickle
 import json
 import sys
@@ -8,22 +8,38 @@ import requests
 from requests import *
 
 class Command:
-    def __init__(self, command):
+    def __init__(self, command, path, context):
         self.id = ObjectId().__str__()
         self.command = command
         self.created = time()*1000
+        self.context = context
+        self.path = path
 
     def to_JSON(self):
         return jsonpickle.encode(self)
 
 
+class User_Context:
+    def __init__(self, process_id, start_time, user_id, system_id):
+        self.process_id = long(process_id)
+        self.start_time = mktime(strptime(start_time, "%c"))
+        self.user_id = user_id
+        self.system_id = system_id
+
+    def to_JSON(self):
+        return jsonpickle.encode(self)
+
 if __name__== "__main__":
 
     if len(sys.argv) > 1:
-        command = Command(sys.argv[1])
-        url = "http://bashhub.com/command"
+        args = sys.argv
+        context = User_Context(long(args[4]), args[5], args[2], args[3])
+        command = Command(args[1], args[6], context)
+        #url = "http://bashhub.com/command"
+        url = "http://localhost:9000/command"
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         try:
             r = requests.post(url, data=command.to_JSON(), headers=headers)
+           # print command.to_JSON()
         except ConnectionError as error:
             pass
--- src/shell/bashhub.sh
+++ src/shell/bashhub.sh
@@ -1,8 +1,15 @@
 update_prompt()
 {
+    PROCESS_ID=$$
+    PROCESS_START=$(ps -p $$ -o lstart | sed -n 2p)
+    USER_ID="52364ac5b52d605f31a78c44"
+    SYSTEM_ID="52364af1b52d605f4a3b49e9"
+    WORKING_DIRECTORY=$(pwd)
+
     if [[ $1 != "bash_command=" ]]
     then
-        ~/.bashhub/bashhub.py "$1"
+        ~/git/bashhub-client/src/python/bashhub.py "$1" $USER_ID $SYSTEM_ID $PROCESS_ID \
+            "$PROCESS_START" "$WORKING_DIRECTORY"
     fi;
 }
 trap 'previous_command=$this_command; this_command=$BASH_COMMAND' DEBUG
