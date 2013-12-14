426be1984197fd63fc27ae03d0671074063cbfcf
Ryan Caloras <rcaloras@gmail.com>
2013-12-14 15:25:57 -0500

    Moved model out of main file

--- src/python/bashhub.py
+++ src/python/bashhub.py
@@ -1,42 +1,10 @@
 #!/usr/bin/python
-from bson.objectid import ObjectId
-from time import *
-import jsonpickle
-import json
 import sys
 from requests import ConnectionError
 import requests
 from bashhub_globals import *
-
-class Command(object):
-    def __init__(self, command, path, context):
-        self.id = ObjectId().__str__()
-        self.command = command
-        self.created = time()*1000
-        self.context = context
-        self.path = path
-
-    def to_JSON(self):
-        return jsonpickle.encode(self)
-
-    @staticmethod
-    def from_JSON(json):
-        return jsonpickle.decode(json)
-
-class User_Context(object):
-    def __init__(self, process_id, start_time, user_id, system_id):
-        self.process_id = long(process_id)
-        self.start_time = mktime(strptime(start_time.strip(), "%c"))
-        self.user_id = user_id
-        self.system_id = system_id
-
-    def to_JSON(self):
-        return jsonpickle.encode(self)
-
-    @staticmethod
-    def from_JSON(json):
-        return jsonpickle.decode(json)
-
+sys.path.insert(0, 'model')
+from Command import *
 
 if __name__== "__main__":
     if len(sys.argv) > 1:
