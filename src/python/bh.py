d60d5b6ca0e239085f9b2453810ae8225a58f0d2
Ryan Caloras <rcaloras@gmail.com>
2013-12-15 16:26:14 -0500

    Added try catch for connection error

--- src/python/bh.py
+++ src/python/bh.py
@@ -5,6 +5,7 @@ import jsonpickle
 import json
 import sys
 import requests
+from requests import ConnectionError
 import cli.app
 sys.path.insert(0, 'model')
 from Command import *
@@ -15,9 +16,16 @@ from bashhub_globals import *
 def bh(app):
     payload = {'userId' : BH_USER_ID, 'limit' : app.params.number}
     url = BH_URL + "/command/last"
-    r = requests.get(url, params=payload)
-    commandsJson = r.json()
-    for command in reversed(commandsJson):
+    try:
+        r = requests.get(url, params=payload)
+        print_commands(reversed(r.json()))
+
+    except ConnectionError as error:
+        print "Sorry, looks like there's a connection error. Please try again later"
+
+
+def print_commands(commands_json):
+    for command in (commands_json):
         minCommand = MinCommand.from_JSON(json.dumps(command))
         print minCommand
 
