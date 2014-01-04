de7e42a1875ce40b340255be2e2b79844ffc6954
Ryan Caloras <rcaloras@gmail.com>
2014-01-04 03:48:33 -0500

    Added if statement for bashhub_globals
    - Just need to set BH_URL if you want to do dev environment

--- src/python/bashhub_globals.py
+++ src/python/bashhub_globals.py
@@ -5,6 +5,10 @@ pulled in from environment variables or are just used across multiple files.
 
 import os
 
-BH_URL = "http://bashhub.com"
+
+# Should put this into an optional environment variable
+BH_URL = "http://bashhub.com" if "BH_URL" not in os.environ.keys() \
+        else os.environ["BH_URL"]
+
 BH_USER_ID = os.environ["BH_USER_ID"]
 BH_SYSTEM_ID = os.environ["BH_SYSTEM_ID"]
