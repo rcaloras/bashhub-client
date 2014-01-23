26472088b898c6199ba3d1ed0e6ddee47a480924
Ryan Caloras <rcaloras@gmail.com>
2014-01-22 22:19:54 -0500

    Changed command input
    -Removed debug trap. Should eventually use this entirely, but right now
     PROMPT_COMMAND is easier.
    -Can't enter the same command multiple times. We only record it once.
     Eventually handle this case and create options for it.
    -Can now handle hashtags.
    -Fixed a bug where it was entering commands over and over for submitting
     empty prompts.

--- src/shell/bashhub.sh
+++ src/shell/bashhub.sh
@@ -1,24 +1,31 @@
 source ~/.bashhub/.config
 
-bh_process_command()
+BH_PROCESS_COMMAND()
 {
-    PROCESS_ID=$$
-    PROCESS_START=`ps -p $$ -o lstart | sed -n 2p`
-    USER_ID=$BH_USER_ID
-    SYSTEM_ID=$BH_SYSTEM_ID
-    WORKING_DIRECTORY=`pwd`
-
-    if [[ $1 != "bash_command=" ]]
+    # Check to make sure we have a new command
+    if [[ $BH_PREV_HISTORY = $BH_RAW_HISTORY ]];
     then
-        PARSED_COMMAND=`echo $1 | sed -e 's/^ *//g' -e 's/ *$//g'`
-        (cd $BH_EXEC_DIRECTORY && ./bashhub.py "$PARSED_COMMAND" $USER_ID $SYSTEM_ID $PROCESS_ID \
-           "$PROCESS_START" "$WORKING_DIRECTORY"&)
+        exit 0;
     fi;
+
+    local BH_COMMAND=$(echo "$BH_RAW_HISTORY" |  cut -d " " -f4-)
+    local PROCESS_ID=$$
+    local PROCESS_START=`ps -p $$ -o lstart | sed -n 2p`
+    local USER_ID=$BH_USER_ID
+    local SYSTEM_ID=$BH_SYSTEM_ID
+    local WORKING_DIRECTORY=`pwd`
+
+    # Had this previously, think it was part of the trap. DELETE ME later if
+    # still not useful :)
+    #local PARSED_COMMAND=`echo "$BH_COMMAND" | sed -e 's/^ *//g' -e 's/ *$//g'`
+
+    (cd $BH_EXEC_DIRECTORY && ./bashhub.py "$BH_COMMAND" $USER_ID $SYSTEM_ID $PROCESS_ID \
+        "$PROCESS_START" "$WORKING_DIRECTORY"&)
 }
 
-trap 'previous_command=$this_command; this_command=$(history 1 | cut -d " " -f4-)' DEBUG
-PROMPT_COMMAND='bash_command=$previous_command; if [[ $bash_command ]]; then (cd $BH_EXEC_DIRECTORY && bh_process_command "$bash_command"); bash_command=; fi;'
-
+PROMPT_COMMAND='BH_PREV_HISTORY=$BH_RAW_HISTORY;
+                BH_RAW_HISTORY=$(history 1);
+                (BH_PROCESS_COMMAND);'
 bh()
 {
     (cd $BH_EXEC_DIRECTORY && ./bh.py "$@")
