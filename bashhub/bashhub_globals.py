e51d3cf85ea6ce59bc3f4eb6c981125e510b025d
Ryan Caloras <rcaloras@gilt.com>
2014-06-15 14:41:15 -0400

    Updated Globals to '' for system and user
    - During install system and user are not set. Set them to empty string if we
      can't find them.
    
    - Probably come back and add some logging.

--- bashhub/bashhub_globals.py
+++ bashhub/bashhub_globals.py
@@ -5,12 +5,20 @@ pulled in from environment variables or are just used across multiple files.
 
 import os
 
+def get_global_from_env(env_var, default):
+    default if env_var not in os.environ.keys() \
+            else os.environ(env_var)
+
 
 # Optional environment variable to configure for development
-BH_URL = "http://bashhub.com" if "BH_URL" not in os.environ.keys() \
-        else os.environ["BH_URL"]
+# export BH_URL='http://localhost:9000'
+BH_URL = get_global_from_env('BH_URL', 'http://bashhub.com')
 
-BH_USER_ID = os.environ["BH_USER_ID"]
-BH_SYSTEM_ID = os.environ["BH_SYSTEM_ID"]
+BH_USER_ID = get_global_from_env('BH_USER_ID', '')
+
+BH_SYSTEM_ID = get_global_from_env('BH_SYSTEM_ID', '')
+
 BH_HOME = "~/.bashhub/" if "HOME" not in os.environ.keys() \
         else os.environ["HOME"] + '/.bashhub'
+
+
