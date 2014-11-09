d8054bd50ee9d502de93093b8a5e4bee150f2e44
Ryan Caloras <rcaloras@alumni.cmu.edu>
2014-11-09 14:48:59 -0500

    Removed some stuff from the install header.

--- install-bashhub.sh
+++ install-bashhub.sh
@@ -4,16 +4,9 @@
 #
 # Ryan Caloras (ryan@bashhub.com)
 #
-# It must work everywhere, including on systems that lack
-# a /bin/bash, map 'sh' to ksh, ksh97, bash, ash, or zsh,
-# and potentially have either a posix shell or bourne
-# shell living at /bin/sh.
-#
-# See this helpful document on writing portable shell scripts:
 # http://www.gnu.org/s/hello/manual/autoconf/Portable-Shell.html
 #
 # The only shell it won't ever work on is cmd.exe.
-
 
 bash_profile_hook='
 ### Bashhub.com Installation
