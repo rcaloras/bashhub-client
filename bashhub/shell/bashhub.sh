ed7a127cee0af6af53a51cd9440d23be37673df6
Ryan Caloras <rcaloras@gmail.com>
2014-11-15 20:45:27 -0500

    WIP probably makes sense to pull these together at some point.
    Figure there might be some separate things we want to do in the prompt command
    function though.

--- bashhub/shell/bashhub.sh
+++ bashhub/shell/bashhub.sh
@@ -1,57 +1,12 @@
 source ~/.bashhub/.config
+source ~/.bashhub/lib-bashhub.sh
+
 export BH_HOME_DIRECTORY="$HOME/.bashhub/"
 export BH_EXEC_DIRECTORY="$HOME/.bashhub/env/bin"
 
+
 # Alias to bind Ctrl + B
 bind '"\C-b":"\C-u\C-kbh -i\n"'
-
-BH_PROCESS_COMMAND()
-{
-
-    local BH_COMMAND=$@
-
-    # Check to make sure we have a new command
-    if [[ $BH_PREV_COMMAND = $BH_COMMAND ]];
-    then
-        exit 0;
-    fi;
-
-    # Check to make sure we have valid tokens
-    if [[ -z "$BH_USER_ID" ]] || [[ -z "$BH_SYSTEM_ID" ]];
-    then
-        exit 0;
-    fi;
-
-    # Check to make sure bashhub is still installed. Otherwise, this will
-    # simply fail and spam the user that files dont exist.
-    if [[ ! -f $BH_EXEC_DIRECTORY/bashhub ]];
-    then
-        exit 0;
-    fi;
-
-    local PROCESS_ID=$$
-
-    # This is non-standard across systems. GNU Date and BSD Date
-    # both convert to epoch differently. Using python for cross system
-    # compatibility.
-    local PROCESS_START_STAMP
-    PROCESS_START_STAMP=$(ps -p $$ -o lstart | sed -n 2p)
-
-    local PROCESS_START=$($BH_EXEC_DIRECTORY/bashhub util parsedate "$PROCESS_START_STAMP")
-
-    local WORKING_DIRECTORY=$(pwd)
-
-    ($BH_EXEC_DIRECTORY/bashhub save "$BH_COMMAND" "$WORKING_DIRECTORY" \
-    "$PROCESS_ID" "$PROCESS_START"&)
-}
-
-BH_TRIM_WHITESPACE() {
-    local var=$@
-    var="${var#"${var%%[![:space:]]*}"}"   # remove leading whitespace characters
-    var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespace characters
-    echo -n "$var"
-}
-
 BH_GET_LAST_COMMAND() {
     # GRAB LAST OF COMMAND
     local HISTORY_LINE=$(history 1)
--- bashhub/shell/bashhub.zsh
+++ bashhub/shell/bashhub.zsh
@@ -1,46 +1,7 @@
 source ~/.bashhub/.config
+source ~/.bashhub/lib-bashhub.sh
 export BH_HOME_DIRECTORY="$HOME/.bashhub/"
 export BH_EXEC_DIRECTORY="$HOME/.bashhub/env/bin"
-
-BH_PROCESS_COMMAND()
-{
-
-    local BH_COMMAND=$@
-
-    # Check to make sure we have a new command
-    if [[ $BH_PREV_COMMAND = $BH_COMMAND ]];
-    then
-        exit 0;
-    fi;
-    
-    # Check to make sure we have valid tokens
-    if [[ -z "$BH_USER_ID" ]] || [[ -z "$BH_SYSTEM_ID" ]];
-    then
-        exit 0;
-    fi;
-
-    # Check to make sure bashhub is still installed. Otherwise, this will
-    # simply fail and spam the user that files dont exist.
-    if [[ ! -f $BH_EXEC_DIRECTORY/bashhub ]];
-    then
-        exit 0;
-    fi;
-
-    local PROCESS_ID=$$
-
-    # This is non-standard across systems. GNU Date and BSD Date
-    # both convert to epoch differently. Using python for cross system
-    # compatibility.
-    local PROCESS_START_STAMP
-    PROCESS_START_STAMP=$(ps -p $$ -o lstart | sed -n 2p)
-
-    local PROCESS_START=$($BH_EXEC_DIRECTORY/bashhub util parsedate "$PROCESS_START_STAMP")
-
-    local WORKING_DIRECTORY=$(pwd)
-
-    ($BH_EXEC_DIRECTORY/bashhub save "$BH_COMMAND" "$WORKING_DIRECTORY" \
-    "$PROCESS_ID" "$PROCESS_START"&)
-}
 
 function bh_preexec() {
     BH_PREV_COMMAND=$BH_COMMAND;
