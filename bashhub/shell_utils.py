5289769c042f8c7fb748f97bc30aa9057fdbc2ed
Ryan Caloras <rcaloras@stage60.giltqa.com>
2015-03-01 16:55:30 +0000

    Made build_user_context work with python 2.6
    - Had to add '0' for the string format.

--- bashhub/shell_utils.py
+++ bashhub/shell_utils.py
@@ -10,7 +10,7 @@ def build_user_context():
     # Non standard across systems GNU Date and BSD Date
     # both convert to epoch differently. Need to use
     # python date util to make standard.
-    start_time_command = "ps -p {} -o lstart | sed -n 2p".format(ppid)
+    start_time_command = "ps -p {0} -o lstart | sed -n 2p".format(ppid)
     date_string = os.popen(start_time_command).read().strip()
     date = dateutil.parser.parse(date_string)
     start_time = int(mktime(date.timetuple()))*1000
