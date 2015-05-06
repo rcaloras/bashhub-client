c1e746293915882fb9d724219d55c317f32b5dd0
Ryan Caloras <rcaloras@gmail.com>
2015-05-06 00:07:05 -0400

    Prepare for release 0.0.12

--- install-bashhub.sh
+++ install-bashhub.sh
@@ -35,7 +35,7 @@ zshprofile=~/.zshrc
 
 # Optional parameter to specify a github branch
 # to pull from.
-github_branch=${1:-'0.0.11'}
+github_branch=${1:-'0.0.12'}
 
 install_bashhub() {
     check_dependencies
