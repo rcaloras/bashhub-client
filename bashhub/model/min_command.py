a839b085ffe57d8519c0d457734afc1e93294e49
Ryan Caloras <rcaloras@gmail.com>
2015-08-03 22:07:30 -0400

    Add uuid to MinCommand

--- bashhub/model/min_command.py
+++ bashhub/model/min_command.py
@@ -6,9 +6,10 @@ from serializable import Serializable
 
 class MinCommand(Serializable):
 
-    def __init__(self, command, created):
+    def __init__(self, command, created, uuid):
         self.command = command
         self.created = created
+        self.created = uuid
 
     def __str__(self):
         return self.command.encode('utf8')
