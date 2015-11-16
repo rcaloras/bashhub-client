e4d59326d3b9f15293b26545840186edb2461702
Ryan Caloras <rcaloras@gmail.com>
2015-11-15 21:48:45 -0500

    Using os.getenv with default value.
    - Didn't realize this was default in python :P

--- bashhub/bashhub_globals.py
+++ bashhub/bashhub_globals.py
@@ -5,20 +5,15 @@ pulled in from environment variables or are just used across multiple files.
 
 import os
 
-def get_global_from_env(env_var, default):
-    return default if env_var not in os.environ.keys() \
-            else os.environ[env_var]
-
-
 # Optional environment variable to configure for development
 # export BH_URL='http://localhost:9000'
-BH_URL = get_global_from_env('BH_URL', 'https://bashhub.com')
+BH_URL = os.getenv('BH_URL', 'https://bashhub.com')
 
-BH_USER_ID = get_global_from_env('BH_USER_ID', '')
+BH_USER_ID = os.getenv('BH_USER_ID', '')
 
-BH_SYSTEM_ID = get_global_from_env('BH_SYSTEM_ID', '')
+BH_SYSTEM_ID = os.getenv('BH_SYSTEM_ID', '')
 
-BH_HOME = "~/.bashhub" if "HOME" not in os.environ.keys() \
-        else os.environ["HOME"] + '/.bashhub'
+BH_HOME = '~/.bashhub' if 'HOME' not in os.environ.keys() \
+        else os.environ['HOME'] + '/.bashhub'
 
 
