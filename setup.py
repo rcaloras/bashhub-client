3431c0f090930b5f937bb10d1a7bbdd09463c565
Ryan Caloras <rcaloras@gmail.com>
2017-12-10 17:59:42 -0500

    Adding tests_require for dev env setup

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import sys
 
 exec (open('bashhub/version.py').read())
 
+tests_require = ['pytest>=3.3.1']
+
 setup(name='bashhub',
       version='__version__',
       description='Bashhub.com python client',
@@ -16,8 +18,10 @@ setup(name='bashhub',
       install_requires=[
           'requests==1.2.3', 'jsonpickle==0.7.0', 'click==3.3',
           'npyscreen==4.9.1', 'python-dateutil==2.4', 'pyCLI==2.0.3',
-          'pymongo==2.6', 'inflection==0.2.1', 'humanize==0.5.1'
+          'pymongo==2.6', 'inflection==0.2.1', 'humanize==0.5.1',
       ],
+      tests_require=tests_require,
+      extras_require={'test': tests_require},
       entry_points={
           'console_scripts': ['bh=bashhub.bh:main',
                               'bashhub=bashhub.bashhub:main']
