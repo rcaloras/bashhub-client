16dc6cf45c9300dbd4263c6c2fbcda86a99db9d1
Ryan Caloras <rcaloras@alumni.cmu.edu>
2018-12-31 19:37:52 -0500

    Fixing install version number

--- install-bashhub.sh
+++ install-bashhub.sh
@@ -41,7 +41,7 @@ zshprofile=~/.zshrc
 
 # Optional parameter to specify a github branch
 # to pull from.
-github_branch=${1:-'1.20.0'}
+github_branch=${1:-'1.2.0'}
 
 install_bashhub() {
     check_dependencies
