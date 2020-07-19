b11482c8c7c77e0fd59363a3a7b75cadf206cd5c
Ryan Caloras <rcaloras@gmail.com>
2020-07-18 23:51:47 -0400

    Prepare for release 2.0.1

--- install-bashhub.sh
+++ install-bashhub.sh
@@ -45,7 +45,7 @@ zshprofile=~/.zshrc
 
 # Optional parameter to specify a github branch
 # to pull from.
-github_branch=${1:-'2.0.0'}
+github_branch=${1:-'2.0.1'}
 
 install_bashhub() {
     check_dependencies
