9cb306ad18df9657042d8a84466e6a50dee04b3b
Ryan Caloras <rcaloras@gmail.com>
2020-09-21 16:43:52 -0400

    Prepare for release 2.1.0

--- install-bashhub.sh
+++ install-bashhub.sh
@@ -52,7 +52,7 @@ fish_config="${XDG_CONFIG_HOME:-~/.config}/fish/config.fish"
 
 # Optional parameter to specify a github branch
 # to pull from.
-github_branch=${1:-'2.0.2'}
+github_branch=${1:-'2.1.0'}
 
 install_bashhub() {
     check_dependencies
