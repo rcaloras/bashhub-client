6fb04413ba2132e2d0b27e6ecff6a6980eeb71de
Ryan Caloras <rcaloras@alumni.cmu.edu>
2022-07-18 11:46:04 -0400

    Prepare for release 2.3.0

--- install-bashhub.sh
+++ install-bashhub.sh
@@ -68,7 +68,7 @@ fish_config="${XDG_CONFIG_HOME:-~/.config}/fish/config.fish"
 
 # Optional parameter to specify a github branch
 # to pull from.
-github_branch=${1:-'2.2.0'}
+github_branch=${1:-'2.3.0'}
 
 install_bashhub() {
     check_dependencies
