27b75185dfb96b1cf6344d7f4a79c16839457a46
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2023-02-24 10:07:59 -0500

    Remove fragile and broken exec for standard import
    Using `exec` to import the version is anti-pattern, unpythonic, and fragile. What's more, it does not work, because the Python interpreter is looking in the CWD for the `bashhub/version.py` whereas it actually needs to look in the script location. The script location is found using the ugly method below:
    
    ```python3
    import os
    import sys
    
    exec (open(os.path.join(os.path.dirname(sys.argv[0]),'./bashhub/version.py')).read())
    ```

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup, find_packages
 import install_bashhub
-import sys
-
-exec (open('bashhub/version.py').read())
+from bashhub.version import __version__
 
 tests_require = ['pytest>=3.3.1']
 
 setup(name='bashhub',
-      version='__version__',
+      version=__version__,
       description='Bashhub.com python client',
       url='https://github.com/rcaloras/bashhub-client',
       author='Ryan Caloras',
@@ -19,7 +17,7 @@ setup(name='bashhub',
           'requests==2.23.0', 'jsonpickle==2.0.0', 'click==6.7',
           'npyscreen==4.10.5', 'python-dateutil==2.8.1',
           'pymongo==3.10.1', 'inflection==0.3.1', 'humanize==1.0.0',
-          'future==0.18.3', 'mock==3.0.5'
+          'future==0.18.2', 'mock==3.0.5'
       ],
       tests_require=tests_require,
       extras_require={'test': tests_require},
