77ad21bfcf8f9e1961df6ed28b3075e8922705db
Tony Scelfo <scelfo@gmail.com>
2023-12-28 15:07:43 -0700

    Update status.py to use the configured bashhub host

--- bashhub/view/status.py
+++ bashhub/view/status.py
@@ -1,23 +1,24 @@
 import dateutil.parser
 import datetime
 import humanize
+from ..bashhub_globals import BH_URL
 
 status_view = """\
 === Bashhub Status
-https://bashhub.com/{0}
-Total Commands: {1}
-Total Sessions: {2}
-Total Systems:  {3}
+{0}{1}
+Total Commands: {2}
+Total Sessions: {3}
+Total Systems:  {4}
 ===
-Session PID {4} Started {5}
-Commands In Session: {6}
-Commands Today: {7}
+Session PID {5} Started {6}
+Commands In Session: {7}
+Commands Today: {8}
 """
 
 def build_status_view(model):
     date = datetime.datetime.fromtimestamp(model.session_start_time / 1000.0)
     date_str = humanize.naturaltime(date)
     return status_view.format(
-        model.username, model.total_commands, model.total_sessions,
+        BH_URL, model.username, model.total_commands, model.total_sessions,
         model.total_systems, model.session_name, date_str,
         model.session_total_commands, model.total_commands_today)
