5439a107af0f508247e3a62a32ad817887d57cc7
Ryan Caloras <rcaloras@alumni.cmu.edu>
2026-08-29 14:28:57 -0400

    Support editable installs with SCM versions

--- bashhub/version.py
+++ bashhub/version.py
@@ -1,11 +1,10 @@
 import platform
+from importlib.metadata import version as installed_version
 
 try:
     from ._version import __version__
 except ImportError:
-    # Source checkouts do not contain the build-generated version module.
-    from setuptools_scm import get_version
-
-    __version__ = get_version(root="..", relative_to=__file__)
+    # Editable installs load this source module without the wheel's generated file.
+    __version__ = installed_version("bashhub")
 
 version_str = 'Bashhub {0} (python {1})'.format(__version__, platform.python_version())
