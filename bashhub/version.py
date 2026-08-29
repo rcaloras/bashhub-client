6461a617ff2b38993bfc8a0bb78d0d303d43157b
Ryan Caloras <rcaloras@alumni.cmu.edu>
2026-08-29 14:20:34 -0400

    Derive package versions from Git

--- bashhub/version.py
+++ bashhub/version.py
@@ -1,5 +1,11 @@
 import platform
 
-__version__ = '3.1.0'
+try:
+    from ._version import __version__
+except ImportError:
+    # Source checkouts do not contain the build-generated version module.
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
 
 version_str = 'Bashhub {0} (python {1})'.format(__version__, platform.python_version())
